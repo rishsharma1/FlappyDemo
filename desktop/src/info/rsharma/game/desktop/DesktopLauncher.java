844da6c680647e3e3ca150e4207a611a5347283f
Rishabh Sharma <rishsharma13@gmail.com>
2017-02-06 22:07:55 +1100

    add project files

---
+++ desktop/src/info/rsharma/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package info.rsharma.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.rsharma.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
